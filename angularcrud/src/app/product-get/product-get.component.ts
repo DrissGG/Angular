beeec1e5ce59c78edfeb9454d05f76d639f7cc36
Driss <drissgazzaoui1986@hotmail.com>
2024-09-06 12:29:00 +0200

    buton click supprimer et modifier

--- angularcrud/src/app/product-get/product-get.component.ts
+++ angularcrud/src/app/product-get/product-get.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from '../products.service';
 import { ProductInterface } from '../interfaces/product.interface';
+import { Router } from '@angular/router'; // Importez le Router
 
 @Component({
   selector: 'app-product-get',
@@ -13,7 +14,7 @@ import { ProductInterface } from '../interfaces/product.interface';
 export class ProductGetComponent {
   products: ProductInterface[] = [];
 
-  constructor(private productsService: ProductsService) { }
+  constructor(private productsService: ProductsService, private router: Router) { } // Injectez le Router
 
   ngOnInit() {
     this.productsService.loadProducts().subscribe({
@@ -31,12 +32,17 @@ export class ProductGetComponent {
     this.productsService.deleteProduct(id).subscribe({
       next: (product) => {
         console.log('Produit supprimé avec succès:', product);
-        // Filtrer le produit supprimé de la liste
         this.products = this.products.filter((p) => p.id !== id);
       },
       error: (error) => {
         console.error(`Erreur lors de la suppression du produit :`, error);
       },
     });
+  }
+
+  // Nouvelle méthode pour gérer la redirection vers l'édition
+  handleEditProduct(id: string) {
+    console.log(`Redirection vers la page de modification pour le produit avec l'ID: ${id}`);
+    this.router.navigate(['/edit', id]); // Redirige vers le chemin '/edit/:id'
   }
 }
